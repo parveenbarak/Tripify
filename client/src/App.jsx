b9e7a0f797eda58db6ddf9ce5e762d7d3a47dd10
parveen barak <parveenbarak117@gmail.com>
2024-11-09 22:04:49 +0530

    frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,44 @@
-import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import './App.css'
-import Footer from './components/Footer'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
-import { SignIn,SignUp } from '@clerk/clerk-react'
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import { PageNotFound } from "./pages/PageNotFound";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import Navbar from "./pages/Navbar";
+import { Pricing } from "./pages/Pricing";
+import { Footer } from "./pages/Footer";
+import CityDetails from "./city/[cityid]";
+import GenerateTrip from "./generate-trip/GenerateTrip";
+import FinalTrip from "./trip/[tripid]";
+import AllTours from "./alltours/AllTours";
+
 function App() {
- 
+  const { isLoaded, isSignedIn } = useUser();
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isSignedIn && isLoaded) {
+      navigate("/auth");
+    }
+  }, [isSignedIn, isLoaded, navigate]);
 
   return (
-    <Router>
+    <>
+      <Navbar />
       <Routes>
-        
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </Router>
-
-   
-  )
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/alltours" element={<AllTours />} />
+        <Route path="/city/:cityid" element={<CityDetails />} />
+        <Route path="/generatetrip" element={<GenerateTrip />} />
+        <Route path="/trip/:tripid" element={<FinalTrip />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
